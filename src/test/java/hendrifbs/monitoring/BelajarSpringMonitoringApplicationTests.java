c1cc4ce41dd2e6a0272fc563cb500f947180894a
MHendriF <hendrifebriansyah28@gmail.com>
2025-10-01 15:15:22 +0800

    init project

---
+++ src/test/java/hendrifbs/monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package hendrifbs.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
